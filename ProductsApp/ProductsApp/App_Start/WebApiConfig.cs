d5708133ed776ae4ac7529892da6499bff39a112
sdep2580 <sdep2580@gmail.com>
2017-03-05 23:02:16 +0800

    一隻獨立的API

--- ProductsApp/ProductsApp/App_Start/WebApiConfig.cs
+++ ProductsApp/ProductsApp/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace BookService
+namespace ProductsApp
 {
     public static class WebApiConfig
     {
--- ProductsApp/ProductsApp/Properties/AssemblyInfo.cs
+++ ProductsApp/ProductsApp/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 組件的一般資訊會透過將設定 
+// 組件的一般資訊是由下列的屬性集 
 // 控制。變更這些屬性值可修改與組件關聯的
 // 資訊。
-[assembly: AssemblyTitle("BookService")]
+[assembly: AssemblyTitle("ProductsApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BookService")]
-[assembly: AssemblyCopyright("Copyright (C)  2017")]
+[assembly: AssemblyProduct("ProductsApp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible 為 false 的方式來控制，讓此組件中的類型在 
-// COM 組件中為不可見。如果您需要從
-// COM 存取此組件中的型別，請在該型別上將 ComVisible 屬性設定為 true。
+// 將 ComVisible 設定為 false 會使得這個組件中的型別 
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取此組件中 
+// 的型別，請在該型別上將 ComVisible 屬性設定為 true。
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("66497106-70b3-4602-bc18-55422b7c9273")]
+[assembly: Guid("08e067b3-9316-424a-afb5-62ac81766979")]
 
 // 組件的版本資訊是由下列四項值構成:
 //
 //      主要版本
-//      次要版本存取此組件中的類型，
+//      次要版本 
 //      組建編號
 //      修訂編號
 //
-// 您可以指定所有值或預設修訂和組件數目
+// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將修訂和組建編號 
 // 指定為預設值:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
